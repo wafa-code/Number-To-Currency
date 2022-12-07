eb6bba7b9bb2e186aff8a1c6fa466d126e52283f
wafa .h <36425720+wafa-code@users.noreply.github.com>
2022-12-07 23:00:51 +0100

    Update NumberToCurrency.java

--- NumberToCurrency.java
+++ NumberToCurrency.java
@@ -2,7 +2,6 @@ import java.text.NumberFormat;
 
 public class NumberToCurrency {
 
-// Convert Number to Algerian Currency ----> Example: 600000000 ==> 600,000,000.00 DZD
   public static String NumtoCurrency(Number money){
     String value = "";
     NumberFormat formatter = NumberFormat.getCurrencyInstance();
@@ -12,7 +11,7 @@ public class NumberToCurrency {
 }  
 
 public static void main(String[] args) {
-    // Convert Number to Algerian Currency
+    // Convert Number to Algerian Currency ----> Example: 600000000 ==> 600,000,000.00 DZD
     System.out.println("Currency : "+NumtoCurrency(600000000));
 }
 }
